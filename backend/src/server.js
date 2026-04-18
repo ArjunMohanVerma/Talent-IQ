28f79ab02d3b5deb7b746da2b9687907ee7f52fe
Arjun <arjunmohanvermaamv@gmail.com>
2026-04-18 12:23:09 +0530

    correction

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ import { clerkMiddleware } from "@clerk/express";
 
 import {ENV} from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
-import { inngest, functions } from "./lib/inngest.js";
+import { inngest } from "./lib/inngest.js";
 
 const app = express();
 

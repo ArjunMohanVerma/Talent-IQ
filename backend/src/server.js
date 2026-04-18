ed593ee2858fcbe79d2b9faf52c7541a3844e492
Arjun <arjunmohanvermaamv@gmail.com>
2026-04-18 10:50:53 +0530

    tools configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,25 @@
 import express from "express";
 import path from "path";
 import cors from "cors";
+import { serve } from "inngest/express";
+import { clerkMiddleware } from "@clerk/express";
 
 import {ENV} from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
+import { inngest, functions } from "./lib/inngest.js";
 
 const app = express();
 
 const _dirname = path.resolve();
 
 app.use(express.json());
+app.use(clerkMiddleware()); 
 app.use(cors({
     credentials:true,
     origin:ENV.CLIENT_URL
 }));
 
-app.get("/", (req,res)=>{
-    res.send("Welcome to home page")
-})
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 if(ENV.NODE_ENV === "production"){
     app.use(express.static(path.join(_dirname, "../frontend/dist")));
